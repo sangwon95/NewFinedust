3b4417562daaf4f96a7f377c75340364b0eb4c6c
pagsang-won <tkddnjs2575@naver.com>
2023-10-14 17:11:23 +0900

    23.10.14 update

---
+++ app/src/main/java/com/tobie/newfinedust/adapter/RemainAdapter.kt
@@ -0,0 +1,35 @@
+package com.tobie.newfinedust.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.tobie.newfinedust.R
+import com.tobie.newfinedust.models.Remain
+
+class RemainAdapter(remainList: ArrayList<Remain>) : RecyclerView.Adapter<RemainAdapter.ViewHolder>() {
+    var dataList: ArrayList<Remain> = remainList
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val txtTitle: TextView = view.findViewById(R.id.tv_title)
+        val txtStatus: TextView = view.findViewById(R.id.tv_status)
+        val txtValue: TextView = view.findViewById(R.id.tv_value)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.remain_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+      return dataList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = dataList[position]
+        holder.txtTitle.text = item.title
+        holder.txtStatus.text = item.status
+        holder.txtValue.text = item.value
+    }
+}
---
+++ app/src/main/java/com/tobie/newfinedust/models/Remain.kt
@@ -0,0 +1,4 @@
+package com.tobie.newfinedust.models
+
+// 미세먼지, 초미세먼지 제외한 나머지
+data class Remain(val title: String, val status: String, val value: String)
